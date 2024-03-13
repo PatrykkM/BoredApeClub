1bb15a5d3cee7dcf45c77bbc490415069b2d8857
PatrykkM <patrykmrzyglod3@gmail.com>
2024-03-13 15:44:43 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 import ErrorPage from "./components/layouts/ErrorPage/ErrorPage";
 import Footer from "./components/layouts/Footer";
@@ -7,23 +8,25 @@ import Header from "./components/layouts/Header/Header";
 import ErrorBuyingNFT from "./components/layouts/MainPage/ErrorBuyingNFT";
 import MainPage from "./components/layouts/MainPage/MainPage";
 import MyNFTsPage from "./components/layouts/MyNftPage/MyNFTsPage";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 
 function App() {
 	return (
 		<Provider store={store}>
-			<BrowserRouter>
-				<div className="box-border flex flex-col items-center justify-center overflow-hidden bg-main-background font-kanit">
-					<Header />
-					<Routes>
-						<Route path="/BoredApeClub/" element={<MainPage />} />
-						<Route path="/BoredApeClub/MyNFTs" element={<MyNFTsPage />} />
-						<Route path="/*" element={<ErrorPage />} />
-					</Routes>
-					<Footer />
-					<ErrorBuyingNFT />
-				</div>
-			</BrowserRouter>
+			<PersistGate persistor={persistor}>
+				<BrowserRouter>
+					<div className="box-border flex flex-col items-center justify-center overflow-hidden bg-main-background font-kanit">
+						<Header />
+						<Routes>
+							<Route path="/BoredApeClub/" element={<MainPage />} />
+							<Route path="/BoredApeClub/MyNFTs" element={<MyNFTsPage />} />
+							<Route path="/*" element={<ErrorPage />} />
+						</Routes>
+						<Footer />
+						<ErrorBuyingNFT />
+					</div>
+				</BrowserRouter>
+			</PersistGate>
 		</Provider>
 	);
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,45 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { AllNFTSlice } from "./slices/AllNFTsSlice";
 import { CurrentBalanceSlice } from "./slices/CurrentBalanceSlice";
 import { FAQSlice } from "./slices/FaqSlice";
 import { MyNFTSlice } from "./slices/MyNftSlice";
 
+const myNFTPersistConfig = {
+	key: "HandleNFT",
+	storage,
+};
+
+const persistedMyNFTReducer = persistReducer(myNFTPersistConfig, MyNFTSlice.reducer);
+const persistedCurrentBalanceSlice = persistReducer(
+	myNFTPersistConfig,
+	CurrentBalanceSlice.reducer,
+);
+
 export const store = configureStore({
 	reducer: {
-		HandleNFT: MyNFTSlice.reducer,
+		HandleNFT: persistedMyNFTReducer,
 		FAQSlice: FAQSlice.reducer,
-		CurrentBalanceSlice: CurrentBalanceSlice.reducer,
+		CurrentBalanceSlice: persistedCurrentBalanceSlice,
 		AllNFTS: AllNFTSlice.reducer,
 	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [
+					"persist/PERSIST",
+					"persist/REHYDRATE",
+					"persist/PAUSE",
+					"persist/PURGE",
+					"persist/REGISTER",
+				],
+			},
+		}),
 });
+
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
